737455b638f228ed4f4147fe572f04d45b92a7e3
MSquared88 <39702456+MSquared88@users.noreply.github.com>
2019-09-06 11:32:50 -0500

    cards showing with info

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,46 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+//components
+import PlayerCard from './components/PlayerCards'
+
+import { Header, Card, } from 'semantic-ui-react'
+
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      players: []
+
+    }
+  }
+  
+  componentDidMount() {
+    axios
+    .get('http://localhost:5000/api/players')
+    .then(res => {
+      // console.log(res.data)
+      this.setState({
+        players: res.data})
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header as='h1'>First Header</Header>
+        <Card.Group itemsPerRow={6}>
+         {this.state.players.map(player => {
+           return(
+             <PlayerCard player= {player}/>
+           )
+         })}
+         
+        </Card.Group>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ client/src/components/PlayerCards.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Card, Image, Icon } from 'semantic-ui-react'
+import picture from '../jeffrey-f-lin-r3V5-rfFErY-unsplash.jpg'
+
+function PlayerCard(props) {
+	let searchStr = props.player.name.replace(/\s+/g, '_') 
+	
+	return(
+		<Card>
+			<Image src= {picture}/>
+			<Card.Content>
+				<Card.Header>{props.player.name}</Card.Header>
+					<Card.Meta>
+						<span className='country'>Country: {props.player.country}</span>
+					</Card.Meta>
+			</Card.Content>
+				<Card.Content extra>
+					<a 
+					href= {`https://en.wikipedia.org/wiki/${searchStr}`}
+					style= {{fontWeight: '600', color: 'black'}}
+					>
+						wikipedia Link
+					</a>
+				</Card.Content>
+			</Card>
+	)
+}
+
+export default PlayerCard
